aae99cd8ce5cb4d4394d87d869f91884fdba014c
Zakaria Hasan <zakaria.raz@gmail.com>
2019-09-21 19:26:20 +0300

    Initial commit

---
+++ webServer.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+
+class Serv(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path =='/':
+            self.path= '/index.html'
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.end_headers()
+            self.wfile.write(bytes(file_to_open,'utf-8'))
+
+httpd = HTTPServer(('localhost',8080),Serv)
+httpd.serve_forever()
